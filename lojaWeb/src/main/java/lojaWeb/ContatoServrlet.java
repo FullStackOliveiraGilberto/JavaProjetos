b49a09619d8dfcdc253e50de997cd4a8704e1057
Gilberto Oliveira <skwcontadores@gmail.com>
2019-02-23 22:19:24 -0300

    Maven projetos testes
    ALura

---
+++ lojaWeb/src/main/java/lojaWeb/ContatoServrlet.java
@@ -0,0 +1,28 @@
+package lojaWeb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns={"/contato"})
+public class ContatoServrlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	
+		
+		
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html><h2>Ligue para asfasfdaf@@@: </h2> </html>");
+		writer.close();
+	}
+	
+}
